ce1d7f2fa3f31436057875f51f04e69834daddc6
21120447 <haonhat2729@gmail.com>
2024-08-08 12:08:33 +0700

    :bug: fix: button title 3

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Calendar mode="single" selected={date} onSelect={setDate} className="rounded-md border" />
-      <Button>Click me</Button>
+      <Button>Click</Button>
       <Button>Click me</Button>
     </main>
   );
